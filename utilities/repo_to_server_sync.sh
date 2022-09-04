e4aa0e687580a6401b6d2f31dfc82009e523d7cb
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2022-09-04 16:58:56 -0700

    Add comments for sync plan

--- utilities/repo_to_server_sync.sh
+++ utilities/repo_to_server_sync.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
 echo "Let the syncing begin..."
+
+## Create Backup on ODU CS Server
+
+## Update main with latest Website Code
+
+## sftp Website Code to ODU CS Server
+
+## Alert that Copy has been Completed
