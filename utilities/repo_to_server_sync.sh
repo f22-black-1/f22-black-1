c6b16f8a1cbee4cebb266b5e898fe3c862de3044
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2022-09-05 16:48:12 -0700

    Create sync script

--- utilities/repo_to_server_sync.sh
+++ utilities/repo_to_server_sync.sh
@@ -1,11 +1,43 @@
 #!/usr/bin/bash
 
+########################################################################
+## A utility for syncing GitHub main code line to ODU CS Linux Server
+##
+##
+## Prerequisites for using this script: 
+## > passwordless ssh to server
+## > VPN access (if not on campus)
+## > For help in getting that setup, please contact @anton
+########################################################################
+
+#TODO: Add proper logging, error handling, and email-alerting
+
 echo "Let the syncing begin..."
+DATE=$(date "+%Y%m%d")
+HOME_DIR="/Users/ant0n" # Provide your own $HOME dir
+REPO_DIR="$HOME_DIR/Documents/Repos/f22-black-1/" # Provide your own repo directory
+SSH_PRIVATE_KEY_FILE="$HOME_DIR/.ssh/antonMac"  # Provide your own private key
 
 ## Create Backup on ODU CS Server
+echo "Creating backup..."
+BACKUP_LOC="website_backups/$DATE/secure_html/"
+ssh -i $SSH_PRIVATE_KEY_FILE -o IdentitiesOnly=yes -t 411black@linux.cs.odu.edu "mkdir -p $BACKUP_LOC && cp -R secure_html $BACKUP_LOC"
+echo "Backup complete..."
 
 ## Update main with latest Website Code
+echo "Update main branch to get latest Website code..."
+mkdir $HOME_DIR/repo_clone_tmp
+cd $HOME_DIR/repo_clone_tmp
+git clone "git@github.com:f22-black-1/f22-black-1.git"
 
 ## sftp Website Code to ODU CS Server
+echo "Copying website to ODU CS Server..."
+scp -i $SSH_PRIVATE_KEY_FILE -r $HOME_DIR/repo_clone_tmp/f22-black-1/website/secure_html 411black@linux.cs.odu.edu:/home/411black/
+echo "Copying website to ODU CS Server complete!"
 
 ## Alert that Copy has been Completed
+
+## Clean Up
+echo "Cleaning up..."
+rm -rf $HOME_DIR/repo_clone_tmp/
+echo "Program complete."
