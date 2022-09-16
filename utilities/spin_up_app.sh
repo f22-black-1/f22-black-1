28b9a18f946a722ddc938c2ef7dbc21ce57e53a1
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2022-09-16 00:03:50 -0700

    Get DB Dockerized and run queries with back end

--- utilities/spin_up_app.sh
+++ utilities/spin_up_app.sh
@@ -11,16 +11,11 @@ nohup docker build -t pest_patrol_app_client:1.0 .  > ~/log.out &
 
 nohup docker run -p 4200:4200 pest_patrol_app_client:1.0  >> ~/log.out &
 
-# Spin up Back End
+# Spin up Back End and Database
 
 cd $REPO_ROOT/application/server
 
-nohup docker build -t pest_patrol_app_server:1.0 .  >> ~/log.out &
-
-nohup docker run -p 8080:8080 pest_patrol_app_server:1.0  >> ~/log.out &
-
-# Spin up Data Base
-
+nohup docker compose up >> ~/log.out &
 
 cd $REPO_ROOT/application && tail -f ~/log.out
 
