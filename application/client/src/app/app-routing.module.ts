ecf508eaf19d4c8aea9238a2e358d11bfd5e39ca
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2022-09-27 21:56:07 -0700

    Update for merge demo

--- application/client/src/app/app-routing.module.ts
+++ application/client/src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PestsComponent } from './pests/pests.component';
 import { PestDetailComponent } from './pest-detail/pest-detail.component';
-import { PostRequestTypedComponent } from './post-request/post-request.component';
+import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'map', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: PestDetailComponent },
   { path: 'pests', component: PestsComponent },
   { path: 'pests/:id', component: PestsComponent },
-  //{ path: 'test', component: PostRequestTypedComponent }
+  { path: 'map', component: MapComponent },
 ];
 
 @NgModule({
--- application/client/src/app/app.module.ts
+++ application/client/src/app/app.module.ts
@@ -1,27 +1,37 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PestDetailComponent } from './pest-detail/pest-detail.component';
 import { PestsComponent } from './pests/pests.component';
+import { MapComponent } from './map/map.component';
+
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     PestsComponent,
     PestDetailComponent,
+    MapComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    GoogleMapsModule
+    
+  ],
+  exports: [
+    GoogleMapsModule
   ],
   bootstrap: [ AppComponent ]
 })
