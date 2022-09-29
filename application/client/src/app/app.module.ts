e4e9d7db70b787dc058be9d99794fb7835fad7d9
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2022-09-29 01:10:05 -0700

    Initial goofs with Google Maps API and Angular's wrapper

--- application/client/src/app/app.module.ts
+++ application/client/src/app/app.module.ts
@@ -1,17 +1,17 @@
 
 import { NgModule } from '@angular/core';
-
+import { GoogleMapsModule } from '@angular/google-maps'
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { GoogleMapsModule } from '@angular/google-maps'
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { PestDetailComponent } from './pest-detail/pest-detail.component';
-import { PestsComponent } from './pests/pests.component';
 import { MapComponent } from './map/map.component';
+import { PestsComponent } from './pests/pests.component';
+import { PestDetailComponent } from './pest-detail/pest-detail.component';
 import { ForumComponent } from './forum/forum.component';
 // import { LoginComponent } from './login/login.component';
 // import { RegisterComponent } from './register/register.component';
--- application/client/src/app/map/map.component.ts
+++ application/client/src/app/map/map.component.ts
@@ -1,24 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-// https://timdeschryver.dev/blog/google-maps-as-an-angular-component
+import {Component, ViewChild, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import {MapInfoWindow, MapMarker} from '@angular/google-maps';
+
+// See: https://github.com/angular/components/blob/main/src/google-maps/README.md
+
 @Component({
-  selector: 'app-googlemap',
+  selector: 'map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  @ViewChild(MapInfoWindow)
+  infoWindow!: MapInfoWindow;
+  apiLoaded: Observable<boolean>;
 
-  constructor() { }
-
-  // marker = new google.maps.Marker({
-  //   position: odu,
-  //   map: map,
-  // });
-
-  ngOnInit(): void {
+  // TODO: Try to determine what this HttpClient does
+  constructor(httpClient: HttpClient) {
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyCVbhnCSH7RXZW3ojx_mqB0IZe6FfOkA6Q', 'callback')
+        .pipe(
+          map(() => true),
+          catchError(() => of(false)),
+        );
   }
+
   display : any;
   center: google.maps.LatLngLiteral = {lat: 36.8862699, lng: -76.3097248 };
   zoom = 15;
+
+  markerPositions: google.maps.LatLngLiteral[] = [];
+  markerOptions: google.maps.MarkerOptions = {
+    label: {
+      color: 'green',
+      text: 'Marker label',
+    },
+    animation: google.maps.Animation.BOUNCE
+  };
+
+
+
+  ngOnInit(): void {
+  }
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    this.markerPositions.push(event.latLng!.toJSON());
+    console.log(`Added marker to ${event.latLng!}` )
+  }
 
   moveMap(event: google.maps.MapMouseEvent) {
     if(event.latLng!= null)
@@ -28,6 +56,11 @@ export class MapComponent implements OnInit {
   move(event: google.maps.MapMouseEvent) {
     if(event.latLng != null)
     this.display = event.latLng.toJSON();
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    this.infoWindow.open(marker);
+    console.log("Opening an info window...")
   }
 
 
