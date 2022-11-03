706663ecad34ba7265c9767c1462b308f288df37
TheMan23123 <vvega001@odu.edu>
2022-11-03 16:28:44 -0400

    Pest Info now shows up in text boxes when give ID

--- application/client/src/app/pest-update/pest-update.component.ts
+++ application/client/src/app/pest-update/pest-update.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Pest } from '../pest';
+import { PestDetailComponent } from '../pest-detail/pest-detail.component';
+import { PestService } from '../pest.service';
+
 @Component({
   selector: 'app-pest-update',
   templateUrl: './pest-update.component.html',
   styleUrls: ['./pest-update.component.css']
 })
 export class PestUpdateComponent implements OnInit {
+  pest: Pest = {
+    pestid: "",
+    pestname: "",
+    pesttype: "",
+    severity: "",
+    pestdescription: "",
+    pestimage: ""
+  }
 
   constructor() { }
-
+ 
   ngOnInit(): void {
   }
 
+  getPestID(val: string){
+    this.pest.pestid = val;
+    
+  }
 }
