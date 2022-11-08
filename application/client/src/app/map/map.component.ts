a54491ffd9fdfacab2b8b19e07dd699fe4e900a7
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2022-11-07 21:58:27 -0800

    Complete Populating Pests to Incident Map

--- application/client/src/app/map/map.component.ts
+++ application/client/src/app/map/map.component.ts
@@ -8,11 +8,17 @@ import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { PestReportComponent } from '../pest-report/pest-report.component';
 import { PestService } from '../pest.service';
 import * as mapStyle from "./map.component.style.json";
+// TODO: Merge Pest and Incident Data Structures
 import { Pest, Incident} from '../pest';
-//import { PestLocation } from '../pest';
+
 
 // See: https://github.com/angular/components/blob/main/src/google-maps/README.md
 
+
+class ReportedPest {
+  position: google.maps.LatLngLiteral = null;
+  options: google.maps.MarkerOptions = {icon: null};
+}
 
 @Component({
   selector: 'map',
@@ -22,8 +28,6 @@ import { Pest, Incident} from '../pest';
 
 
 export class MapComponent implements OnInit {
-  
-
   constructor(
     private  dialogRef : MatDialog,
     private pestService: PestService
@@ -42,76 +46,111 @@ export class MapComponent implements OnInit {
     disableDoubleClickZoom: true,
     maxZoom: 20,
     minZoom: 12,
-    //gestureHandling: "none",
     styles: mapStyle
   };
 
-  iconBase = 'https://cs.odu.edu/~411black/assets/';
+  iconBase = '../../assets/icons/';
   display!: google.maps.LatLngLiteral;
 
   move(event: google.maps.MapMouseEvent) {
-    
+
       this.display = event.latLng.toJSON();
-    
   }
-
-
-  createMarker(options: google.maps.MarkerOptions): google.maps.Marker {
-
-    const marker = new google.maps.Marker(options)
-    return marker;
-
-  }
-
 
   markers: google.maps.Marker[] = [];
   marker: google.maps.Marker;
- 
+
   markerInfoContent = '';
   openInfoWindow(marker: MapMarker, position: google.maps.LatLngLiteral) { 
-    // this is called when the marker is clicked.
-
     this.infoWindow.open(marker);
-
-    console.log(`Marker positions: {${position.lat}, ${position.lng}}`)
+    console.log(`Info Window for Marker positions: {${position.lat}, ${position.lng}}`)
   }
 
-
-  markerPositions: google.maps.LatLngLiteral[] = [];
-  markerIcon: string;
-
-  markerOptions: google.maps.MarkerOptions = {
+  markerOptions: google.maps.MarkerOptions[] = [{
     draggable: false,
-    animation: google.maps.Animation.DROP
-  };
+    animation: google.maps.Animation.DROP,
+    icon: ""
+  }];
 
-  addVisibleMarker(event: google.maps.MapMouseEvent) {
-         
-    this.markerPositions.push(event.latLng!.toJSON());
-    console.log(`Added marker to ${event.latLng!}` )
+  reportedPests: ReportedPest[] = [];
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    let position = event.latLng!.toJSON()
+
+    let reportedPest = new ReportedPest();
+    reportedPest.position = position;
+
+    // TODO: Don't let the marker be visible until after it has been reported
+    reportedPest.options.icon = null;
+    
+    this.reportedPests.push(reportedPest)
+    
+
+    console.log(`Manually added marker to ${position.lat}, ${position.lng}` )
    
  }
 
  Incidents: Array<Object> = [];
  incident: Incident
-  getIncidents(): Array<Object> {
+ position: google.maps.LatLngLiteral;
+  getIncidents(): any {
     this.pestService.getIncidents().subscribe( async data => {
       const incidents = await data;
-      let position = google.maps.LatLng;
-    
 
-      //console.log(incidents[0].incidentid)
       for (let i=0; i < incidents.length; i++) {
-        // console.log(incidents[i]);
         this.Incidents.push(incidents[i]);
-        position = (incidents[i].xcoord, incidents[i].ycoord);
-        console.log(position);
-                // this.markerPositions.push();
-        // console.log(`Added marker to ${event.latLng!}` )
+        this.position = {lat: incidents[i].xcoord, lng: incidents[i].ycoord};
+        
+        console.log(`Added ${incidents[i].pestid} marker to ${this.position.lat}, ${this.position.lng}` )
+          // TODO: Set icon based on pest type
+        if (incidents[i].pestid == '55884984-3891-4c9c-b8d0-8526c65d5588') {
+          let reportedPest = new ReportedPest();
+          reportedPest.position = this.position;
+          reportedPest.options.icon = this.iconBase + 'bobcat.png'
+          this.reportedPests.push(reportedPest)
+        }
+        else if (incidents[i].pestid == 'b0b6c409-f67d-4646-be93-0bd4d1111ff4') {
+          let reportedPest = new ReportedPest();
+          reportedPest.position = this.position;
+          reportedPest.options.icon = this.iconBase + 'ant.png'
+          this.reportedPests.push(reportedPest)
+        }
+        else if (incidents[i].pestid == '1304fec9-e2ac-4f72-8e74-2450d1a03abb') {
+          let reportedPest = new ReportedPest();
+          reportedPest.position = this.position;
+          reportedPest.options.icon = this.iconBase + 'rat.png'
+          this.reportedPests.push(reportedPest)
+        }
+        else if (incidents[i].pestid == '7a532d09-6b6f-4050-a8a5-2b367441e31d') {
+          let reportedPest = new ReportedPest();
+          reportedPest.position = this.position;
+          reportedPest.options.icon = this.iconBase + 'bee.png'
+          this.reportedPests.push(reportedPest)
+        }
+        else if (incidents[i].pestid == 'a875fb40-3909-4610-a159-6e5bca823476') {
+          let reportedPest = new ReportedPest();
+          reportedPest.position = this.position;
+          reportedPest.options.icon = this.iconBase + 'wasp.png'
+          this.reportedPests.push(reportedPest)
+        }
+        else if (incidents[i].pestid == 'bd898a91-7b36-4442-8e41-479c5f951a20') {
+          let reportedPest = new ReportedPest();
+          reportedPest.position = this.position;
+          reportedPest.options.icon = this.iconBase + 'cougar.png'
+          this.reportedPests.push(reportedPest)
+        }
+        else {
+          let reportedPest = new ReportedPest();
+          console.log("Not setting marker icon")
+          reportedPest.position = this.position;
+          this.reportedPests.push(reportedPest);
+        }
+
       }
-      
+      console.log(this.reportedPests)
     
     })
+    
     return this.Incidents
   };
      
@@ -119,56 +158,20 @@ export class MapComponent implements OnInit {
 
   getReportAndSetMarker(event: google.maps.MapMouseEvent) {
     console.log(`Getting report....`)
-    this.addVisibleMarker(event) 
+    this.addMarker(event) 
     this.dialogRef.open(PestReportComponent, {
       height: '33%',
       width: '33%'
-  });
+    });
 
   }
   
-
-
-
   ngOnInit() {
 
     console.log(`Centering map on ${this.mapCenter.lat}, ${this.mapCenter.lng}`);
-    this.getIncidents();
-    // let incidents = this.getIncidents();
-
-    // console.log(incidents)
-
+    let incidents = this.getIncidents();
+    
   }
 
-  
-  
 
-  
 }
-
-
-      // // Need to have pesttype here so we can if-check the icon
-      // console.log(incidents)
-      // if (incidents[incident].pestid == '55884984-3891-4c9c-b8d0-8526c65d5588') {
-      //   options.icon = this.iconBase + 'donatello.png';
-      //   options.position = {lat: xcoord, lng: ycoord};
-      //   marker = this.createMarker(options)
-      //   this.markerPositions.push(options.position)
-      //   this.markers.push(marker)
-      //   this.markerInfoContent = incidents[incident].reportdate 
-      // }
-      // if (incidents[incident].pestid == 'b0b6c409-f67d-4646-be93-0bd4d1111ff4') {
-      //   options.icon = this.iconBase + 'raphael.png';
-      //   options.position = {lat: xcoord, lng: ycoord};
-      //   marker = this.createMarker(options)
-      //   this.markerPositions.push(options.position)
-      //   this.markers.push(marker)
-      //   this.markerInfoContent = incidents[incident].reportdate 
-      // }
-      // else {
-      //   options.position = {lat: xcoord, lng: ycoord};
-      //   marker = this.createMarker(options)
-      //   this.markerPositions.push(options.position)
-      //   this.markers.push(marker)
-      //   this.markerInfoContent = incidents[incident].reportdate
-      // }
