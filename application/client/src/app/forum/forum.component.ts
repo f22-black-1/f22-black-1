aceb2d12f7f0c672e3b608d2ea159071bc636009
unknown <aunde00@odu.edu>
2022-11-12 19:41:38 -0600

    moved filter com to forum com, gave forum access to filter value

--- application/client/src/app/forum/forum.component.ts
+++ application/client/src/app/forum/forum.component.ts
@@ -27,6 +27,9 @@ export class ForumComponent implements OnInit {
     this.getThreads();
   }
 
+  //
+  //this plugs into the backend
+  //goes to the summary Thread service
   getThreads(): Array<SummaryThread_Prev> {
     this.summaryThreadService.getThreads()
     .subscribe(stlp => this.summaryThreadList = stlp);
@@ -146,5 +149,12 @@ export class ForumComponent implements OnInit {
     this.stl2.push(st3);
   }
 
+  //see pestOptionArray of filter componet for all of pest types
+  pestTypeSelected: string = "ALL";
+  
+  onRadioButtonChanged(pestType: string){
+    this.pestTypeSelected = pestType;
+    console.log(this.pestTypeSelected + " forum onRadioButtonChanged")
+  }
 
 }
